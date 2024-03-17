479fa0399e8344b1c8a618385292068b93b5ce5c
AGX <55353943+agx-r@users.noreply.github.com>
2024-03-18 00:50:16 +0500

    Браузер открывается в полноэкранном режиме автоматически, улучшено отображение.

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
+import webbrowser
 from flask import Flask, render_template
 import yfinance as yf
+import pyautogui, time
 
 app = Flask(__name__)
+browser_opened = False
+
+def open_browser_on_localhost():
+    print("BROWSER STARTED")
+    global browser_opened
+    if not browser_opened:
+        url = "http://localhost:5000"
+        webbrowser.open(url)
+        time.sleep(1)
+        pyautogui.press('f11')
+        browser_opened = True
 
 @app.route('/')
 def home():
@@ -16,4 +29,5 @@ def get_quote(symbol):
         return {'error': str(e)}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    open_browser_on_localhost()
+    app.run(debug=False)
